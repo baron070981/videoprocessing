817628ca7121af227e2668e3eb64c6fa2b9076d2
baron070981 <baron070981@gmail.com>
2020-09-21 16:51:25 +0300

    ----

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+var videoname string = "testfile.mp4"
 
 func download(url string) error {
 	fl, err := os.Create("testfile.mp4")
@@ -22,19 +24,20 @@ func download(url string) error {
 	}
 	defer resp.Body.Close()
 	io.Copy(fl, resp.Body)
-	data, err := ioutil.ReadFile("testfile.mp4")
+	return nil
+}
+
+func getvideo(videoname string, timeframe string) error {
+
+	video, err := os.Open(videoname)
 	if err != nil {
-		fmt.Println("Error read file")
+		fmt.Println("Error read video", err)
 		return err
 	}
-	nd := make([]byte, len(data))
-	for _, b := range data {
-		if b == '\x00' {
-			nd = append(nd, b)
-		}
-	}
+	defer video.Close()
+	videobuff := bufio.NewReader(video)
+	fmt.Println(videobuff)
 
-	fmt.Println(string(nd))
 	return nil
 }
 
@@ -42,5 +45,8 @@ func main() {
 
 	fmt.Println("Start")
 	download("https://whatsaper.ru/wp-content/plugins/download-attachments/includes/download.php?id=1630")
-
+	err := getvideo(videoname, "12:11")
+	if err != nil {
+		fmt.Println("Error from getvideo\n", err)
+	}
 }

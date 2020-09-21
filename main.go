362a2984b917ff052b3dccd3b7d2c0b2036f64af
baron070981 <baron070981@gmail.com>
2020-09-22 00:29:42 +0300

    ------

--- main.go
+++ main.go
@@ -1,52 +1,37 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	"io"
+	//"io"
 	"net/http"
-	"os"
+	//"net/url"
+	"encoding/json"
+	//"os"
 )
 
+// вариант 1
+// составить и отправить запрос
+// 
+
+
+
 var videoname string = "testfile.mp4"
+var urlget string "https://whatsaper.ru/wp-content/plugins/download-attachments/includes/download.php?id=1630"
 
-func download(url string) error {
-	fl, err := os.Create("testfile.mp4")
-	if err != nil {
-		fmt.Println("Error create file...")
-		return err
-	}
-	defer fl.Close()
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error while downloading", url, "-", err)
-		return err
-	}
-	defer resp.Body.Close()
-	io.Copy(fl, resp.Body)
+
+
+func makeRequest() (error){
+	
+	
 	return nil
 }
 
-func getvideo(videoname string, timeframe string) error {
 
-	video, err := os.Open(videoname)
-	if err != nil {
-		fmt.Println("Error read video", err)
-		return err
-	}
-	defer video.Close()
-	videobuff := bufio.NewReader(video)
-	fmt.Println(videobuff)
-
-	return nil
-}
 
 func main() {
 
 	fmt.Println("Start")
-	download("https://whatsaper.ru/wp-content/plugins/download-attachments/includes/download.php?id=1630")
-	err := getvideo(videoname, "12:11")
-	if err != nil {
-		fmt.Println("Error from getvideo\n", err)
-	}
+	
+	
 }
